eafa8a1fd6f255b95ea34a5ca8eb50268adde935
Ilya <admin_imal@mail.ru>
2022-12-08 19:02:34 +0300

    first1

---
+++ src/main/java/lesson1/HW1.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+import java.io.IOException;
+
+public class HW1 {
+
+    public static void main(String[] args) throws IOException {
+
+        ex1();
+
+    }
+
+    private static void ex1() {
+    }
+}
